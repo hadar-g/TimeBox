f03055a313f81a75e64a190db816bf1765e2361a
Hadar Goldstein <hadargoldstein@CDS-HadarG-Mac.local>
2022-06-19 21:34:58 -0400

    fixed timer bugs works now

--- components/FlatTimer.js
+++ components/FlatTimer.js
@@ -11,7 +11,7 @@ const[totalSeconds, setTotalSeconds] = useState(0)
 const [isRunning, setIsRunning] = useState(false)
 
 useEffect(()=> {
-    setTotalSeconds(totalSeconds => (hours*3600) + (mins*60) + (secs))
+    setTotalSeconds( ( Math.floor((parseInt(hours)*3600) + (parseInt(mins)*60) + parseInt(secs) )))
     console.log(hours)
     console.log(mins)
     console.log(secs)
@@ -20,9 +20,13 @@ useEffect(()=> {
 
 useEffect(() => {
 
-    setSecs(secs => ('0' + secs).slice(-2))
-    setMins(mins => ('0' + mins).slice(-2))
-    setHours(hours => ('0' + hours).slice(-2))
+    // setSecs(secs => ('0' + secs).slice(-2))
+    // setMins(mins => ('0' + mins).slice(-2))
+    // setHours(hours => ('0' + hours).slice(-2))
+
+    setSecs(secs => secs.toString().padStart(2, '0'))
+    setMins(mins => mins.toString().padStart(2, '0'))
+    setHours(hours => hours.toString().padStart(2, '0'))
     
     const interval = setInterval(() => {
 
@@ -31,13 +35,25 @@ useEffect(() => {
             
             setTotalSeconds(totalSeconds => totalSeconds - 1);
             console.log(totalSeconds)
+            console.log(hours)
+            console.log(mins)
+            console.log(secs)
 
-            setHours(('0' + (Math.floor(totalSeconds / 3600))).toString().slice(-2))
+            // setHours(('0' + (Math.floor(totalSeconds / 3600)) ).toString().slice(-2) )
+            // setMins(('0' + (Math.floor(totalSeconds % 3600 / 60))).toString().slice(-2) )
+            // setSecs(('0' + (Math.floor(totalSeconds % 3600 % 60))).toString().slice(-2) )
 
-            setMins(('0' + (Math.floor(totalSeconds % 3600 / 60))).toString().slice(-2) )
+            setHours( Math.floor(totalSeconds / 3600) );
+            setMins( Math.floor( (totalSeconds % 3600) / 60) );
+            setSecs( Math.floor( (totalSeconds % 3600) % 60) );
 
-            setSecs(('0' + (Math.floor(totalSeconds %3600 % 60))).toString().slice(-2) )
+            // setSecs(secs => ('0' + secs).slice(-2))
+            // setMins(mins => ('0' + mins).slice(-2))
+            // setHours(hours => ('0' + hours).slice(-2))
 
+            setSecs(secs => secs.toString().padStart(2, '0'))
+            setMins(mins => mins.toString().padStart(2, '0'))
+            setHours(hours => hours.toString().padStart(2, '0'))
 
           //  setSecs(secs => totalSeconds % 60)
         //     setSecs(secs => ('0' + (secs - 1)).slice(-2));
@@ -65,16 +81,28 @@ useEffect(() => {
 
     return(
         <View style = {styles.container}>
-            <Text>{props.title} {hours} : {mins} : {secs} </Text>
+            <Text style = {styles.title}>{props.title}</Text>
+            <Text style = {styles.clock}> {hours} : {mins} : {secs} </Text>
             <Button title ='start' onPress={() => setIsRunning(true)}/>
             <Button title = 'stop' onPress={() => setIsRunning(false)}/>
         </View>
     )
 }
 const styles = StyleSheet.create({
+    title: {
+       // backgroundColor: 'red',
+        flex: 1,
+        fontSize: 18,
+
+    },
+    clock: {
+       // backgroundColor: 'green',
+        flex: 1,
+        fontSize: 18
+    },
     container: {
         flexDirection: 'row',
-        width: '100%',
+        width: '95%',
         //justifyContent: 'center',
         justifyContent: 'left',
         alignItems: 'center',
@@ -82,8 +110,9 @@ const styles = StyleSheet.create({
         borderWidth: 0.5,
         borderRadius: 5,
        // backgroundColor: 'green',
-       padding: 2,
-        margin: 10
+        padding: 2,
+        margin: 10,
+        marginRight: 5
     },
 });
 
